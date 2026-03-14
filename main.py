671e7314df4e5656c57f4449c2a3d27a0b2ab07c
nandomendes <nando.mendes96@gmail.com>
2026-03-14 00:05:35 -0300

    Adicionando calculo ganho mensal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # ...Esta é a parte Backennd do projeto.
 # @author: prof. Nando Mendes
-from flask import  Flask, render_template
+from flask import  Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -12,6 +12,19 @@ def homepage():
 def soma(numero_um, numero_dois):
     resultado = int(numero_um) + int(numero_dois)
     return "O resultado da soma, calculado no Servidor Backend é: " + str(resultado)
+
+@app.route("/calcular_ganho", methods=['POST'])
+def calcular_ganho():
+    valorMensalRecebido = float(request.form['valorMensalRecebido'].replace(',', '.'))
+    valorMensalCombustível = float(request.form['valorMensalCombustível'].replace(',', '.'))
+    valorMensalAlimentacao = float(request.form['valorMensalAlimentacao'].replace(',', '.'))
+    valorMensalImposto = float(request.form['valorMensalImposto'].replace(',', '.'))
+    valorRealGanhoMensal = valorMensalRecebido - valorMensalCombustível - valorMensalAlimentacao - valorMensalImposto
+    return "O valor Real de Ganho Mensal é R$ " + f"{valorRealGanhoMensal:.2f}".replace('.', ',')
+
+@app.route("/form_calcular_ganho")
+def form_calcular_ganho():
+    return render_template("calcular_ganho.html")
 
 if __name__=="__main__":
     app.run(debug=True, port=8080, host='0.0.0.0')
